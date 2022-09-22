5976adb50959c364cc7d25fdda9d948513e45c8d
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-22 09:29:12 +0200

    Add testing in main.java

--- src/hu/petrik/fest/Main.java
+++ src/hu/petrik/fest/Main.java
@@ -1,4 +1,10 @@
 package hu.petrik.fest;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Festmeny f1 = new Festmeny("balls", "ballsgaming360", "painted with his balls");
+        f1.bid(300);
+        System.out.println(f1);
+    }
 }
