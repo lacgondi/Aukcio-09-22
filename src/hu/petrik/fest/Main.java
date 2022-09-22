9f819d502bc9035058aeafcb79d5e7006cc03c08
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-22 10:49:41 +0200

    Add tests
    It now test that you can bid, add to list, and output the objects in said list

--- src/hu/petrik/fest/Main.java
+++ src/hu/petrik/fest/Main.java
@@ -31,14 +31,41 @@ public class Main {
         for (int i = 0; i < num; i++) {
             fList.add(new Festmeny("Festmeny" + i, "Festo" + i, "Stilus" + i));
         }
+        for (int i = 0; i < 20; i++) {
+            fList.get((int) (Math.random() * fList.size())).bid((int) (Math.random() * 100) + 10);
+        }
 
         while (scR.hasNextLine()) {
             String[] temp = scR.nextLine().split(";");
-            fList.add(new Festmeny(temp[0],temp[1],temp[2]));
+            fList.add(new Festmeny(temp[0], temp[1], temp[2]));
         }
 
-        for (int i = 0; i < 20; i++) {
-            fList.get((int) Math.random() * fList.size() - 1).bid((int) (Math.random() * 100) + 10);
+        int paintingID = -1;
+        while (paintingID > fList.size() || paintingID < 0) {
+            System.out.print("Adja meg melyik festményre szeretne licitálni: ");
+            paintingID = sc.nextInt();
+            System.out.print("\n");
+            if (fList.get(paintingID).isSold() == true) {
+                System.out.println("Ez a festmény már elkelt");
+            }
+        }
+        int bid = -1;
+        while (bid < 0) {
+            System.out.print("Mennyit szertne licitálni: ");
+            bid = sc.nextInt();
+            System.out.print("\n");
+            if (bid == 0) {
+                fList.get(paintingID).bid();
+            } else {
+                fList.get(paintingID).bid(bid);
+            }
+        }
+
+        for (int i = 0; i < fList.size(); i++) {
+            if (fList.get(i).getNumberOfBids() > 0) {
+                fList.get(i).setSold(true);
+            }
+            System.out.println(fList.get(i));
         }
 
     }
