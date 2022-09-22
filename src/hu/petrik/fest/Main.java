8ddb38eaf7acb2d3b2351ac1fabb898d7dc5b173
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-22 10:28:21 +0200

    Add read from file function

--- src/hu/petrik/fest/Main.java
+++ src/hu/petrik/fest/Main.java
@@ -11,8 +11,9 @@ public class Main {
     public static void main(String[] args) {
         File fileN = new File("festmenyek.csv");
         Scanner sc = new Scanner(System.in);
+        Scanner scR;
         try {
-            Scanner scR = new Scanner(fileN);
+            scR = new Scanner(fileN);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -28,11 +29,16 @@ public class Main {
         System.out.print("Adja meg, hogy hány festmény adatait adjuk hozzá a listához: ");
         int num = sc.nextInt();
         for (int i = 0; i < num; i++) {
-            fList.add(new Festmeny("Festmeny"+i,"Festo"+i,"Stilus"+i));
+            fList.add(new Festmeny("Festmeny" + i, "Festo" + i, "Stilus" + i));
+        }
+
+        while (scR.hasNextLine()) {
+            String[] temp = scR.nextLine().split(";");
+            fList.add(new Festmeny(temp[0],temp[1],temp[2]));
         }
 
         for (int i = 0; i < 20; i++) {
-            fList.get((int) Math.random()*fList.size()-1).bid((int)(Math.random()*100)+10);
+            fList.get((int) Math.random() * fList.size() - 1).bid((int) (Math.random() * 100) + 10);
         }
 
     }
